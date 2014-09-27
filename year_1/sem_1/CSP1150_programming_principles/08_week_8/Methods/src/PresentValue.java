86b2a54b252db405aac9dfd281327bf02ab32dd3
martymcflywa <doc.brown.83@gmail.com>
2014-09-27 12:43:50 +0800

    CSP1150: finished lab 8

--- year_1/sem_1/CSP1150_programming_principles/08_week_8/Methods/src/PresentValue.java
+++ year_1/sem_1/CSP1150_programming_principles/08_week_8/Methods/src/PresentValue.java
@@ -1,4 +1,4 @@
-// import Scanner, getting user input
+// import Scanner for console user input
 import java.util.Scanner;
 
 /**
@@ -11,30 +11,56 @@ import java.util.Scanner;
  */
 public class PresentValue {
 	
+	// store calculated present value here
 	private static double presentValue;
+	
+	// store user input for futuer value here
 	private static double futureValue;
+	
+	// store user input for interest rate here
 	private static double interestRate;
+	
+	// store user input for years here
 	private static double years;
 	
+	/**
+	 * This is the main method.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		getUserInput();
-		getCalculation(futureValue, interestRate, years);
-		printCalculation(futureValue, interestRate, years, presentValue);
 		
-//		if(years > 0) {
-//			printCalculation(futureValue, interestRate, years, presentValue);
-//		} else {
-//			printZeroYears(years);
-//		}
+		// get user input
+		getUserInput();
+		
+		// get calculation using user input values
+		getCalculation(futureValue, interestRate, years);
+
+		// if years > 0,
+		if(years > 0) {
+			
+			// call printCalculation method to print result
+			printCalculation(futureValue, interestRate, years, presentValue);
+			
+		// else, 
+		} else {
+			
+			// call printZeroYears if they can't wait for at least a year
+			printZeroYears(years);
+		}
 	}
 	
+	/**
+	 * This void method gets user input and
+	 * stores them to the declared variables.
+	 * @param args unused.
+	 */
 	private static void getUserInput() {
 		
 		// create Scanner object
 		Scanner kb = new Scanner(System.in);
 		
 		// print title
-		System.out.println("Bank Deposit Calculation: \n-----------------------");
+		System.out.println("Future Value Calculator: \n-----------------------");
 		
 		// get user input for desired bank balance
 		System.out.println("How much would you like your bank balance to be?");
@@ -52,18 +78,41 @@ public class PresentValue {
 		kb.close();
 	}
 	
+	/**
+	 * This method performs the present value calculation.
+	 * All args are doubles, including years since Math.pow only
+	 * accepts doubles.
+	 * @param double fValue - User input future value.
+	 * @param double intRate - User input interest rate.
+	 * @param double years - User input years.
+	 */
 	private static void getCalculation(double fValue, double intRate, double years) {
 		
-		intRate = intRate + 1;
+		// convert interest rate to usable format + 1
+		intRate = intRate / 100 + 1;
+		
+		// assign presentValue with calculated result
 		presentValue = fValue / Math.pow(intRate, years);
-
 	}
 	
+	/**
+	 * This method prints the result to console.
+	 * Uses printf to format numbers.
+	 * @param double fValue - User input future value.
+	 * @param double intRate - User input interest rate.
+	 * @param double years - User input years.
+	 * @param double pValue - Calculated present value.
+	 */
 	private static void printCalculation(double fValue, double intRate, double years, double pValue) {
-		System.out.println("If you want to earn " + fValue + " and can only wait " + years + " years...");
-		System.out.println("You need to deposit $" + pValue + " into Marty's bank account right now!");
+		System.out.println("-----------------------");
+		System.out.printf("If you want to earn $%.0f and can only wait %.0f years...\n", fValue, years);
+		System.out.printf("you should deposit $%.2f into Marty's bank account right now!", pValue);
 	}
 	
+	/**
+	 * This method prints a message if user inputs zero as years.
+	 * @param double years
+	 */
 	private static void printZeroYears(double years) {
 		System.out.println("You can't wait even a year?...");
 		System.out.println("You could try robbing the bank instead...");
