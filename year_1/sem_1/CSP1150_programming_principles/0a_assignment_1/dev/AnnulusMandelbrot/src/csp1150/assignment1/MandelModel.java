89b85f219056d425bffc98973bbe6a4fa4aba9db
martymcflywa <doc.brown.83@gmail.com>
2014-09-30 12:01:57 +0800

    CSP1150 A1: added mandelbrot subclass

---
+++ year_1/sem_1/CSP1150_programming_principles/0a_assignment_1/dev/AnnulusMandelbrot/src/csp1150/assignment1/MandelModel.java
@@ -0,0 +1,50 @@
+package csp1150.assignment1;
+
+/**
+ * Subclass of CalculatorModel.
+ * The mandelbrot's attributes are saved here,
+ * with default max-min values and overriding
+ * CalculatorModel's isInside() method.
+ * 
+ * @author Martin Ponce ID# 10371381
+ * @version 4.0.0
+ * @since 20140930
+ */
+public class MandelModel extends CalculatorModel {
+	
+	// declaring constant
+	private final int MAX_STEPS = 255;
+	
+	// these variables hold the max-min values
+	private double maxX = 2;
+	private double minX = -2;
+	private double maxY = 2;
+	private double minY = -2;
+	
+	/**
+	 * This method checks if hitpoints are within shape perimeter.
+	 * @param double x - The x-axis value.
+	 * @param double y - The y-axis value.
+	 * @return boolean inside
+	 */
+	protected boolean isInside(double x, double y) {
+		
+		// declare boolean value
+		boolean inside = true;
+		int steps = 0;
+		double px = 0;
+		double py = 0;
+		
+		while(steps < MAX_STEPS && inside) {
+			inside = px * px + py * py < 4.0;
+			
+			double py1 = 2 * px * py + y;
+			px = px * px - py * py + x;
+			py = py1;
+			
+			steps++;
+		}
+		
+		return inside;
+	}
+}
