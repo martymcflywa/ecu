d899dd790b027bc67df1bd3209dad453d72fb133
martymcflywa <doc.brown.83@gmail.com>
2014-10-04 23:50:49 +0800

    CSP1150 A1: reworking base layout

--- year_1/sem_1/CSP1150_programming_principles/0a_assignment_1/dev/AnnulusMandelbrot/src/rewrite/view/CalculatorFrame.java
+++ year_1/sem_1/CSP1150_programming_principles/0a_assignment_1/dev/AnnulusMandelbrot/src/rewrite/view/CalculatorFrame.java
@@ -1,5 +1,35 @@
 package rewrite.view;
 
-public class CalculatorFrame {
+import javax.swing.JFrame;
 
+/**
+ * This class defines the Calculator Frame.
+ * 
+ * @author Martin Ponce ID# 10371381
+ * @version 5.0.0
+ * @since 20141004
+ */
+@SuppressWarnings("serial")
+public class CalculatorFrame extends JFrame {
+	
+	public CalculatorFrame() {
+		
+		// set frame title
+		super("Annulus & Mandelbrot Calculator: Martin Ponce ID# 10371381");
+		
+		// set frame size
+		this.setBounds(100, 100, 800, 900);
+		
+		// disable frame resize
+		this.setResizable(false);
+		
+		// set default action when window is closed
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// set frame to visible
+		this.setVisible(true);
+		
+		// set frame to appear in the center of the desktop
+		this.setLocationRelativeTo(null);
+	}
 }
--- year_1/sem_1/CSP1150_programming_principles/0a_assignment_1/dev/AnnulusMandelbrot/src/rewrite/view/Demo.java
+++ year_1/sem_1/CSP1150_programming_principles/0a_assignment_1/dev/AnnulusMandelbrot/src/rewrite/view/Demo.java
@@ -3,6 +3,19 @@ package rewrite.view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Demo {
 
@@ -36,8 +49,69 @@ public class Demo {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 900, 900);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(new GridLayout(0, 2, 5, 5));
+		
+		JPanel panelLeft = new JPanel();
+		frame.getContentPane().add(panelLeft);
+		panelLeft.setLayout(new GridLayout(2, 1, 20, 5));
+		
+		JPanel panelAnnulusCalc = new JPanel();
+		panelLeft.add(panelAnnulusCalc);
+		GridBagLayout gbl_panelAnnulusCalc = new GridBagLayout();
+		gbl_panelAnnulusCalc.columnWidths = new int[]{0, 0};
+		gbl_panelAnnulusCalc.rowHeights = new int[]{0, 0};
+		gbl_panelAnnulusCalc.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panelAnnulusCalc.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panelAnnulusCalc.setLayout(gbl_panelAnnulusCalc);
+		
+		JPanel panelAnnulusBorder = new JPanel();
+		panelAnnulusBorder.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Annulus Calculator", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_panelAnnulusBorder = new GridBagConstraints();
+		gbc_panelAnnulusBorder.insets = new Insets(20, 20, 10, 0);
+		gbc_panelAnnulusBorder.fill = GridBagConstraints.BOTH;
+		gbc_panelAnnulusBorder.gridx = 0;
+		gbc_panelAnnulusBorder.gridy = 0;
+		panelAnnulusCalc.add(panelAnnulusBorder, gbc_panelAnnulusBorder);
+		
+		JPanel panelMandelbrotCalc = new JPanel();
+		panelLeft.add(panelMandelbrotCalc);
+		GridBagLayout gbl_panelMandelbrotCalc = new GridBagLayout();
+		gbl_panelMandelbrotCalc.columnWidths = new int[]{0, 0};
+		gbl_panelMandelbrotCalc.rowHeights = new int[]{0, 0};
+		gbl_panelMandelbrotCalc.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panelMandelbrotCalc.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panelMandelbrotCalc.setLayout(gbl_panelMandelbrotCalc);
+		
+		JPanel panelMandelbrotBorder = new JPanel();
+		panelMandelbrotBorder.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Mandelbrot Calculator", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_panelMandelbrotBorder = new GridBagConstraints();
+		gbc_panelMandelbrotBorder.insets = new Insets(10, 20, 10, 0);
+		gbc_panelMandelbrotBorder.fill = GridBagConstraints.BOTH;
+		gbc_panelMandelbrotBorder.gridx = 0;
+		gbc_panelMandelbrotBorder.gridy = 0;
+		panelMandelbrotCalc.add(panelMandelbrotBorder, gbc_panelMandelbrotBorder);
+		
+		JPanel panelRight = new JPanel();
+		frame.getContentPane().add(panelRight);
+		panelRight.setLayout(new GridLayout(2, 1, 20, 5));
+		
+		JPanel panelImageAnnulus = new JPanel();
+		panelRight.add(panelImageAnnulus);
+		panelImageAnnulus.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton imageAnnulus = new JButton("Annulus image");
+		panelImageAnnulus.add(imageAnnulus);
+		
+		JPanel panelImageMandelbrot = new JPanel();
+		panelRight.add(panelImageMandelbrot);
+		panelImageMandelbrot.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JButton imageMandelbrot = new JButton("Mandelbrot Image");
+		panelImageMandelbrot.add(imageMandelbrot);
 	}
 
 }
