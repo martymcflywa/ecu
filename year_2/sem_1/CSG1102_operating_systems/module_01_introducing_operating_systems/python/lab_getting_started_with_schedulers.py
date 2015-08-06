a374324b7bfffba40cd6c66d82873b9916d92120
martymcflywa <doc.brown.83@gmail.com>
2015-08-06 10:47:49 +0800

    CSG1102: module 1 - atom modified tabbing

--- year_2/sem_1/CSG1102_operating_systems/module_01_introducing_operating_systems/python/lab_getting_started_with_schedulers.py
+++ year_2/sem_1/CSG1102_operating_systems/module_01_introducing_operating_systems/python/lab_getting_started_with_schedulers.py
@@ -12,75 +12,75 @@ JState = {'Inactive': 1, 'Running': 2, 'Complete': 3}
 
 def main():
 
-    # Create our jobs
-    j1 = Job(1, 14, JState['Inactive'])
-    j2 = Job(2, 7, JState['Inactive'])
-    j3 = Job(3, 19, JState['Inactive'])
+	# Create our jobs
+	j1 = Job(1, 14, JState['Inactive'])
+	j2 = Job(2, 7, JState['Inactive'])
+	j3 = Job(3, 19, JState['Inactive'])
 
-    # Create run queue
-    runQueue = list()
+	# Create run queue
+	runQueue = list()
 
-    # Add our jobs to the run queue
-    runQueue.append(j1)
-    runQueue.append(j2)
-    runQueue.append(j3)
+	# Add our jobs to the run queue
+	runQueue.append(j1)
+	runQueue.append(j2)
+	runQueue.append(j3)
 
-    # Call our scheduler
-    SJF(runQueue)
+	# Call our scheduler
+	SJF(runQueue)
 
-    # Program is complete
-    print("Program Complete")
+	# Program is complete
+	print("Program Complete")
 
 def SJF(runQueue):
 
-    # start at CPU cycle 0
-    cycle = 0
+	# start at CPU cycle 0
+	cycle = 0
 
-    while len(runQueue) > 0:
-        # Set an initial shortest job length
-        shortestTime = runQueue[0].cpuReq
+	while len(runQueue) > 0:
+		# Set an initial shortest job length
+		shortestTime = runQueue[0].cpuReq
 
-        # Set an initial shortest job
-        shortestJob = 0
+		# Set an initial shortest job
+		shortestJob = 0
 
-        # Find shortest job in the run queue
-        for job in runQueue:
-            if (shortestTime > job.cpuReq) and (job.jobState != JState['Complete']):
-                shortestJob = runQueue.index(job)
-                shortestTime = job.cpuReq
+		# Find shortest job in the run queue
+		for job in runQueue:
+			if (shortestTime > job.cpuReq) and (job.jobState != JState['Complete']):
+				shortestJob = runQueue.index(job)
+				shortestTime = job.cpuReq
 
-        # Set shortest job as running
-        runQueue[shortestJob] = runQueue[shortestJob]._replace(jobState = JState['Running'])
+		# Set shortest job as running
+		runQueue[shortestJob] = runQueue[shortestJob]._replace(jobState = JState['Running'])
 
-        # Run active job
-        for x in range(cycle, cycle + runQueue[shortestJob].cpuReq):
-            # Print current cycle
-            print("CPU Cycle #{}".format(x))
+		# Run active job
+		for x in range(cycle, cycle + runQueue[shortestJob].cpuReq):
+			# Print current cycle
+			print("CPU Cycle #{}".format(x))
 
-            # Show current run queue
-            printJobs(runQueue)
+			# Show current run queue
+			printJobs(runQueue)
 
-            # Remove one cycle from required cycles
-            runQueue[shortestJob] = runQueue[shortestJob]._replace(cpuReq = runQueue[shortestJob].cpuReq-1)
+			# Remove one cycle from required cycles
+			runQueue[shortestJob] = runQueue[shortestJob]._replace(cpuReq = runQueue[shortestJob].cpuReq-1)
 
-            # Increment cycle
-            cycle = cycle + 1
-            time.sleep(1)
+			# Increment cycle
+			cycle = cycle + 1
+			time.sleep(1)
 
-        # Mark job as complete
-        runQueue[shortestJob] = runQueue[shortestJob]._replace(jobState = JState['Complete'])
-        print("Job {} complete at CPU Cycle #{}".format(runQueue[shortestJob].id,cycle))
-        printJobs(runQueue)
+		# Mark job as complete
+		runQueue[shortestJob] = runQueue[shortestJob]._replace(jobState = JState['Complete'])
+		print("Job {} complete at CPU Cycle #{}".format(runQueue[shortestJob].id,cycle))
+		printJobs(runQueue)
 
-        # Remove job from run queue
-        runQueue.pop(shortestJob)
+		# Remove job from run queue
+		runQueue.pop(shortestJob)
 
 
 
 def printJobs(runQueue):
-    for job in runQueue:
-        print("Job #{} CPU Required: {} State: {}".format(job.id, job.cpuReq, list(JState.keys())[list(JState.values()).index(job.jobState)]))
-    print()
+	for job in runQueue:
+		print("Job #{} CPU Required: {} State: {}".format(job.id, job.cpuReq, list(JState.keys())[list(JState.values()).index(job.jobState)]))
+	print()
 
 if __name__ == '__main__':
-    main()
+	main()
