ca53e656710d5cc33a6882471c0c91eb471d4f63
martymcflywa <doc.brown.83@gmail.com>
2015-08-29 20:48:35 +0800

    CSG2341 A1a: modified tournamenet to include fuzzyMaxPower

--- year_2/sem_1/CSG2341_intelligent_systems/0b_a1a_gameai_saucers_[30AUG15_2400]/dev/java/SaucersA/src/battle/Tournament.java
+++ year_2/sem_1/CSG2341_intelligent_systems/0b_a1a_gameai_saucers_[30AUG15_2400]/dev/java/SaucersA/src/battle/Tournament.java
@@ -33,8 +33,8 @@ public class Tournament
         // list the combatants here
 
 //        saucers.add(new Saucer(field, new SimpleController()));
-//        saucers.add(new Saucer(field, new FuzzyController()));
-        saucers.add(new Saucer(field, new FuzzyMaxPowerController()));
+        saucers.add(new Saucer(field, new FuzzyController()));
+//        saucers.add(new Saucer(field, new FuzzyMaxPowerController()));
         saucers.add(new Saucer(field, new CheckSixController()));
         
         double[] scores = new double[saucers.size()];
