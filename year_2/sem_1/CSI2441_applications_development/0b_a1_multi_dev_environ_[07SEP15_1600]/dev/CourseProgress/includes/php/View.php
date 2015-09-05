358865f63c617426728834039b4ac648270178e9
martymcflywa <doc.brown.83@gmail.com>
2015-09-05 17:07:02 +0800

    CSI2441 A1: started view superclass

--- year_2/sem_1/CSI2441_applications_development/0b_a1_multi_dev_environ_[07SEP15_1600]/dev/CourseProgress/includes/php/View.php
+++ year_2/sem_1/CSI2441_applications_development/0b_a1_multi_dev_environ_[07SEP15_1600]/dev/CourseProgress/includes/php/View.php
@@ -4,8 +4,23 @@ namespace includes;
 
 class View {
 
-    private $h1Header;
+    protected $h1Header;
+    protected $backButton;
 
+    function __const($h1Header) {
+
+        $this->h1Header = $h1Header;
+        $this->backButton = "<input type=\"button\" name=\"Back\" value=\"Back\" onClick=\"history.go(-1);return true;\">";
+
+    }
+
+    protected final function printH1Header() {
+        echo($this->h1Header);
+    }
+
+    protected final function printBackButton() {
+        echo($this->backButton);
+    }
 }
 
 ?>
