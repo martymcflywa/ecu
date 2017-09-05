0acd774f2c9cc8ba8d527494da5e2764d4a7c71f
marty <doc.brown.83@gmail.com>
2017-09-05 21:59:46 +0800

    Added dispose, converted most functions to methods, switched to table listener.

--- year_2/sem_2/CSP2108_introduction_to_mobile_applications_development/0b_a1_naughts_crosses/dev/NaughtsCrosses/scenes/MainMenu.lua
+++ year_2/sem_2/CSP2108_introduction_to_mobile_applications_development/0b_a1_naughts_crosses/dev/NaughtsCrosses/scenes/MainMenu.lua
@@ -8,40 +8,78 @@ local scene = composer.newScene();
     unless the scene is removed by composer.RemoveScene().
 --]]
 
--- scene objects
-local bg;
-local titleGame;
-local titleMenu;
-local buttonX;
-local buttonO;
-
--- scene object parameters
-local yOffset = _h * 0.2;
-local buttonW = _w * 0.5;
-local buttonH = _h * 0.25;
-local font = "Arial";
-local titleGameText = "xvso";
-local titleMenuText = "choose your weapon";
-local playScreenOptions = {
-    time = 300,
-    params = {}
-};
-
 --[[
-    Defining initializers first.
+    Using table listeners to make it possible to pass playScreenOptions to it.
+    See https://stackoverflow.com/questions/30262822/how-to-use-table-listeners-in-corona-sdk-and-how-it-works
 --]]
-local function initBg(sceneGroup)
+local function playerX(self, event)
+    if(event.phase == "ended") then
+        self.playScreenOptions.effect = "fromLeft";
+        -- pass char to next scene
+        self.playScreenOptions.params.char = _chars[_x];
+        composer.gotoScene("scenes.PlayScreen", self.playScreenOptions);
+    end
+end
+
+local function playerO(self, event)
+    if(event.phase == "ended") then
+        self.playScreenOptions.effect = "fromRight";
+        -- pass char to next scene
+        self.playScreenOptions.params.char = _chars[_o];
+        composer.gotoScene("scenes.PlayScreen", self.playScreenOptions);
+    end
+end
+
+function scene:init(sceneGroup)
+    self.yOffset = _h * 0.2;
+    self.buttonW = _w * 0.5;
+    self.buttonH = _h * 0.25;
+    self.font = "Arial";
+    self.titleGameText = "xvso";
+    self.titleMenuText = "choose your weapon";
+    self.playScreenOptions = {
+        time = 300,
+        params = {}
+    };
+    -- setup background
+    self.bg = self:initBg(sceneGroup);
+    -- setup title objects
+    self.titleGame = self:initTitleGame(sceneGroup);
+    self.titleMenu = self:initTitleMenu(sceneGroup);
+    -- setup buttons
+    local buttonXPosOffset = _cx * 0.5;
+    self.buttonX = self:initButton(sceneGroup, _cx - buttonXPosOffset, _colors["red"], _x);
+    self.buttonX.touch = playerX;
+    self.buttonO = self:initButton(sceneGroup, _cx + buttonXPosOffset, _colors["green"], _o);
+    self.buttonO.touch = playerO;
+    self.buttonX:addEventListener(_event, touch);
+    self.buttonO:addEventListener(_event, touch);
+end
+
+function scene:dispose(sceneGroup)
+    if(sceneGroup ~= nil) then
+        sceneGroup:removeSelf();
+    end
+    if(self.buttonX ~= nil) then
+        self.buttonX:removeEventListener(_event, playerX);
+    end
+    if(self.buttonO ~= nil) then
+        self.buttonO:removeEventListener(_event, playerO);
+    end
+end
+
+function scene:initBg(sceneGroup)
     local bg = _d.newRect(_cx, _cy, _w, _h);
     bg:setFillColor(unpack(_colors["white"]));
     sceneGroup:insert(bg);
     return bg;
 end
 
-local function initTitleGame(sceneGroup)
-    local yOffset = _cy - yOffset;
+function scene:initTitleGame(sceneGroup)
+    local yOffset = _cy - self.yOffset;
     local titleGameOptions = {
-        text = titleGameText,
-        font = font,
+        text = self.titleGameText,
+        font = self.font,
         fontSize = 125,
         align = "center",
         x = _cx,
@@ -53,10 +91,10 @@ local function initTitleGame(sceneGroup)
     return titleGame;
 end
 
-local function initTitleMenu(sceneGroup)
+function scene:initTitleMenu(sceneGroup)
     local titleMenuOptions = {
-        text = titleMenuText,
-        font = font,
+        text = self.titleMenuText,
+        font = self.font,
         fontSize = 25,
         align = "center",
         x = _cx,
@@ -68,23 +106,23 @@ local function initTitleMenu(sceneGroup)
     return titleMenu;
 end
 
-local function initButton(sceneGroup, xPos, color, char)
+function scene:initButton(sceneGroup, xPos, color, char)
     local buttonGroup = _d.newGroup();
-    local yOffset = _cy + yOffset + 50;
+    local yOffset = _cy + self.yOffset + 50;
     buttonGroup.char = char;
     local button = _d.newRect(
         buttonGroup,
         xPos,
         yOffset,
-        buttonW,
-        buttonH
+        self.buttonW,
+        self.buttonH
     );
     button:setFillColor(unpack(color));
 
     local buttonTextOptions = {
         parent = buttonGroup,
         text = char,
-        font = font,
+        font = self.font,
         fontSize = 300,
         align = "center",
         x = xPos,
@@ -92,31 +130,9 @@ local function initButton(sceneGroup, xPos, color, char)
     };
     local buttonText = _d.newText(buttonTextOptions);
     buttonText:setFillColor(unpack(_colors["white"]));
-
+    buttonGroup.playScreenOptions = self.playScreenOptions;
     sceneGroup:insert(buttonGroup);
     return buttonGroup;
-end
-
---[[
-    These listeners are dispatched when x or o button is clicked,
-    allows player to select x or o char with these buttons.
---]]
-local function playerX(event)
-    if(event.phase == "ended") then
-        playScreenOptions.effect = "fromLeft";
-        -- pass char to next scene
-        playScreenOptions.params.char = _chars[_x];
-        composer.gotoScene("scenes.PlayScreen", playScreenOptions);
-    end
-end
-
-local function playerO(event)
-    if(event.phase == "ended") then
-        playScreenOptions.effect = "fromRight";
-        -- pass char to next scene
-        playScreenOptions.params.char = _chars[_o];
-        composer.gotoScene("scenes.PlayScreen", playScreenOptions);
-    end
 end
 
 --[[
@@ -127,15 +143,8 @@ end
 --]]
 function scene:create(event)
     local sceneGroup = self.view;
-    -- setup background
-    bg = initBg(sceneGroup);
-    -- setup title objects
-    titleGame = initTitleGame(sceneGroup);
-    titleMenu = initTitleMenu(sceneGroup);
-    -- setup buttons
-    local buttonXPosOffset = _cx * 0.5;
-    buttonX = initButton(sceneGroup, _cx - buttonXPosOffset, _colors["red"], _x);
-    buttonO = initButton(sceneGroup, _cx + buttonXPosOffset, _colors["green"], _o);
+    self:dispose(sceneGroup);
+    self:init(sceneGroup);
 end
 
 function scene:show(event)
@@ -149,9 +158,6 @@ function scene:show(event)
     --]]
     if(phase == "will") then
         -- do stuff just before shown
-        -- add listeners to the buttons
-        buttonX:addEventListener(_event, playerX);
-        buttonO:addEventListener(_event, playerO);
     --[[
         "did" code executed when scene is completely on screen. Has become the active screen.
         Start transitions, timers, start music for the scene or physics etc.
