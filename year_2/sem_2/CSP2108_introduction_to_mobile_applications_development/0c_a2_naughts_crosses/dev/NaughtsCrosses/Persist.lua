53a59663b06f55fe73bf801932b6ee9069cd268d
Martin Ponce <doc.brown.83@gmail.com>
2017-10-14 07:53:16 +0800

    Fixed Persist when checking for file existence, updated log messages, renamed json file.

--- year_2/sem_2/CSP2108_introduction_to_mobile_applications_development/0c_a2_naughts_crosses/dev/NaughtsCrosses/Persist.lua
+++ year_2/sem_2/CSP2108_introduction_to_mobile_applications_development/0c_a2_naughts_crosses/dev/NaughtsCrosses/Persist.lua
@@ -1,14 +1,23 @@
 local Persist = class("Persist");
 
 function Persist:init()
-    self.filename = "scores.json";
+    self.filename = "score.json";
     self.path = system.DocumentsDirectory;
     self.filepath = system.pathForFile(self.filename, self.path);
 
-    if(not self.filepath) then
+    if(not self:doesFileExist(self.filepath)) then
         self:newFile(self.filepath);
-        self.filepath = system.pathForFile(self.filename, self.path);
     end
+end
+
+function Persist:doesFileExist(filepath)
+    local file = io.open(filepath, "r");
+    if(not file) then
+        return false;
+    end
+    io.close(file);
+    file = nil;
+    return true;
 end
 
 function Persist:loadScores()
@@ -38,11 +47,12 @@ function Persist:read(filepath)
     local file, errorMessage = io.open(filepath, "r");
     local object = nil;
     if(file) then
-        logger:debug(self.name, "read()", string.format("Reading file %s", filepath));
+        logger:debug(self.name, "read()", string.format("Read file: %s", filepath));
         local deserialized = file:read("*a");
+        logger:debug(self.name, "read()", string.format("Deserialize json to object: '%s'", deserialized));
         object = _json.decode(deserialized);
     else
-        logger:debug(self.name, "read()", string.format("Error reading file %s: %s", filepath, errorMessage));
+        logger:debug(self.name, "read()", string.format("Error reading file: %s", errorMessage));
     end
     io.close(file);
     file = nil;
@@ -52,10 +62,10 @@ end
 function Persist:newFile(filepath)
     local file, errorMessage = io.open(filepath, "w");
     if(file) then
-        logger:debug(self.name, "newFile()", string.format("Creating new file %s", filepath));
+        logger:debug(self.name, "newFile()", string.format("Create new file: %s", filepath));
         file:write("");
     else
-        logger:debug(self.name, "newFile()", string.format("Error creating new file %s: %s", filepath, errorMessage));
+        logger:debug(self.name, "newFile()", string.format("Error creating new file: %s", errorMessage));
     end
     io.close(file);
     file = nil;
@@ -66,11 +76,12 @@ function Persist:write(filepath, object)
     local isWrite = false;
     if(file) then
         local serialized = _json.encode(object);
+        logger:debug(self.name, "write()", string.format("Serialize object to json: '%s'", serialized));
         file:write(serialized);
+        logger:debug(self.name, "write()", string.format("Write file: %s", filepath));
         isWrite = true;
     else
-        logger:debug(self.name, "write()", string.format("Error writing file %s: %s", filepath, errorMessage));
-        -- TODO: figure out a way to handle this case gracefully, will need to write file at some point
+        logger:debug(self.name, "write()", string.format("Error writing file: %s", errorMessage));
     end
     io.close(file);
     file = nil;
